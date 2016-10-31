a4c2acbde8a5ae13331842a78d5ef630b0539f48
audiosprite <jpfiorilla@gmail.com>
2016-10-31 18:20:24 -0400

    generates image with flip option

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-const gm = require('gm');
+const gm = require('gm').subClass({imageMagick: true});
 const twit = require('twit');
-const rp = require('request-promise');
 const fs = require('fs');
 
 require.extensions['.txt'] = function (module, filename) {
@@ -8,15 +7,54 @@ require.extensions['.txt'] = function (module, filename) {
 };
 
 const words = require('./wordsnoapostrophes.txt');
-
-// find word, then
-
 var wordsArr = words.split("\n");
 
-// const interpolate = function(word){
-// }
+// find word
+var index = require('./index.txt');
+currentIndex = Number(index);
+console.log('currentIndex', currentIndex, 'word', wordsArr[currentIndex]);
+var currentWord = wordsArr[currentIndex];
 
-// console.log(words);
+// increment index
+const increment = function(){
+    fs.writeFile('./index.txt', (currentIndex+1).toString(), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Index was incremented to " + (currentIndex+1));
+    });
+}
 
-console.log(wordsArr[0]); // string
+var calvinjpg = './calvin.jpg';
 
+// interpolate
+const interpolate = function(word){
+    gm(calvinjpg)
+    // .flop()
+    .font("Arial.ttf", 30)
+    .fill('#000000')
+    .stroke('#000000')
+    .drawText(130, 170, 'testtttttttttttttttt', 'center')
+    .write('./final.jpg', function(err){
+        if (!err) console.log('done');
+    })
+}
+
+const interpolateFlopped = function(word){
+    gm(calvinjpg)
+    .flop()
+    .font("Arial.ttf", 30)
+    .fill('#000000')
+    .stroke('#000000')
+    .drawText(-130, 170, 'testtttttttttttttttt', 'center')
+    .write('./final.jpg', function(err){
+        if (!err) console.log('done');
+    })
+}
+
+increment();
+if (Math.random() > 0.5){
+    interpolate(currentWord);
+} else {
+    interpolateFlopped(currentWord);
+}
